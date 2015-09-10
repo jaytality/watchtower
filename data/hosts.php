f1c47b9778be2d9d1a5f6473ecf9a617470b31ef
Johnathan Tiong <johnathan.tiong@gmail.com>
2015-09-11 09:40:41 +1000

    separated conversion functions to another file

--- data/hosts.php
+++ data/hosts.php
@@ -12,37 +12,8 @@ use ZabbixApi\ZabbixApi;
 // connect to Zabbix API
 $api = new ZabbixApi($settings['zabbix']['url'], $settings['zabbix']['user'], $settings['zabbix']['pass']);
 
-function secondsToTime($seconds) {
-    $dtF = new DateTime("@0");
-    $dtT = new DateTime("@$seconds");
-    return $dtF->diff($dtT)->format('%ad %hh %im %ss');
-}
-
-function bytesToSize($bytes, $precision = 2)
-{
-    $kilobyte = 1024;
-    $megabyte = $kilobyte * 1024;
-    $gigabyte = $megabyte * 1024;
-    $terabyte = $gigabyte * 1024;
-
-    if (($bytes >= 0) && ($bytes < $kilobyte)) {
-        return $bytes . ' B';
-
-    } elseif (($bytes >= $kilobyte) && ($bytes < $megabyte)) {
-        return round($bytes / $kilobyte, $precision) . ' KB';
-
-    } elseif (($bytes >= $megabyte) && ($bytes < $gigabyte)) {
-        return round($bytes / $megabyte, $precision) . ' MB';
-
-    } elseif (($bytes >= $gigabyte) && ($bytes < $terabyte)) {
-        return round($bytes / $gigabyte, $precision) . ' GB';
-
-    } elseif ($bytes >= $terabyte) {
-        return round($bytes / $terabyte, $precision) . ' TB';
-    } else {
-        return $bytes . ' B';
-    }
-}
+// unit conversions
+include_once('../lib/conversion.php');
 
 // stores all the hosts being monitored
 $data['hosts'] = array();
