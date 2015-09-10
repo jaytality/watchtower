7a6ed728ffffbd0f28062e661ccb312c0225c2c4
Johnathan Tiong <johnathan.tiong@gmail.com>
2015-09-10 18:25:40 +1000

    update ajax script

--- data/home.php
+++ data/home.php
@@ -1,5 +1,18 @@
 <?php
 
+error_reporting(E_ALL); // comment out to stop error reporting to browser, notices are not reported!
+ini_set('display_errors', 1);
+
+define('ROOT',dirname(__FILE__));
+require_once('config.php');
+
+// load ZabbixApi
+require_once 'lib/ZabbixApi.class.php';
+use ZabbixApi\ZabbixApi;
+
+// connect to Zabbix API
+$api = new ZabbixApi($settings['zabbix']['url'], $settings['zabbix']['user'], $settings['zabbix']['pass']);
+
 function secondsToTime($seconds) {
     $dtF = new DateTime("@0");
     $dtT = new DateTime("@$seconds");
@@ -165,79 +178,79 @@ try {
     echo $e->getMessage();
 }
 
-// OUTPUT
-$count = 0;
-foreach($data['hosts'] as $host) {
-  ?>
-  <div class="col-sm-4">
-    <div class="well host">
-      <table class="table table-borderless" style="border-bottom: 1px dashed cyan; ">
-        <tr>
-          <th>
-            NAME
-          </th>
-          <td>
-            <?=$host['name']?>
-          </td>
-          <td class="text-right">
-            <?=$host['address'] == '127.0.0.1' ? '45.32.241.7' : $host['address']?>
-          </td>
-        </tr>
-        <tr>
-          <th>
-            UPTIME
-          </th>
-          <td>
-            <?=$host['uptime']?>
-          </td>
-          <td class="text-right">
-            <?php
-              if(isset($host['os'])) {
-                switch($host['os']) {
-                  case "Windows":
-                    echo "WIN";
-                  break;
-
-                  case "Linux":
-                    echo "LIN";
-                  break;
-
-                  case "Darwin":
-                    echo "MAC";
-                  break;
-                }
-              }
-            ?>
-            <?=$host['status'] == true ? '<span style="color: #00ff00; ">ONLINE</span>' : '<span style="color: #ff0000; ">OFFLINE</span>'?>
-          </td>
-        </tr>
-      </table>
-      <div class="clearfix"></div>
-      <div class="row" style="font-size: 1.2rem; ">
-        <!-- CPU -->
-        <div class="col-xs-4 text-center">
-          <span style="font-size: 1.6rem;">CPU</span><br />
-          <?=$host['cpuLoad']. ' %'?>
-        </div>
-
-        <!-- MEMORY -->
-        <div class="col-xs-4 text-center">
-          <span style="font-size: 1.6rem;">RAM</span><br />
-          <?=$host['memory']['free'].' / '.$host['memory']['total']?>
-        </div>
-
-        <!-- DISK -->
-        <div class="col-xs-4 text-center">
-          <span style="font-size: 1.6rem;">HDD</span><br />
-          <?=$host['diskUsage'].' / '.$host['diskTotal']?>
-        </div>
-      </div>
-    </div>
-  </div>
-  <?php
-  $count++;
-  if($count >= 3) {
-    echo '<div class="clearfix"></div>';
+    // OUTPUT
     $count = 0;
-  }
-}
+    foreach($data['hosts'] as $host) {
+        ?>
+        <div class="col-sm-4">
+            <div class="well host">
+                <table class="table table-borderless" style="border-bottom: 1px dashed cyan; ">
+                    <tr>
+                        <th>
+                            NAME
+                        </th>
+                        <td>
+                            <?=$host['name']?>
+                        </td>
+                        <td class="text-right">
+                            <?=$host['address'] == '127.0.0.1' ? '45.32.241.7' : $host['address']?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            UPTIME
+                        </th>
+                        <td>
+                            <?=$host['uptime']?>
+                        </td>
+                        <td class="text-right">
+                            <?php
+                                if(isset($host['os'])) {
+                                    switch($host['os']) {
+                                        case "Windows":
+                                            echo "WIN";
+                                        break;
+
+                                        case "Linux":
+                                            echo "LIN";
+                                        break;
+
+                                        case "Darwin":
+                                            echo "MAC";
+                                        break;
+                                    }
+                                }
+                            ?>
+                            <?=$host['status'] == true ? '<span style="color: #00ff00; ">ONLINE</span>' : '<span style="color: #ff0000; ">OFFLINE</span>'?>
+                        </td>
+                    </tr>
+                </table>
+                <div class="clearfix"></div>
+                <div class="row" style="font-size: 1.2rem; ">
+                    <!-- CPU -->
+                    <div class="col-xs-4 text-center">
+                        <span style="font-size: 1.6rem;">CPU</span><br />
+                        <?=$host['cpuLoad']. ' %'?>
+                    </div>
+
+                    <!-- MEMORY -->
+                    <div class="col-xs-4 text-center">
+                        <span style="font-size: 1.6rem;">RAM</span><br />
+                        <?=$host['memory']['free'].' / '.$host['memory']['total']?>
+                    </div>
+
+                    <!-- DISK -->
+                    <div class="col-xs-4 text-center">
+                        <span style="font-size: 1.6rem;">HDD</span><br />
+                        <?=$host['diskUsage'].' / '.$host['diskTotal']?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+        $count++;
+        if($count >= 3) {
+            echo '<div class="clearfix"></div>';
+            $count = 0;
+        }
+    }
