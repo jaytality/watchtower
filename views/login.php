5c3a47ce7f985d1c4d6d4df34b40488a074e4308
Johnathan Tiong <johnathan.tiong@gmail.com>
2015-09-16 17:46:17 +1000

    syntax correction and button spelling

--- views/login.php
+++ views/login.php
@@ -2,7 +2,7 @@
     <div class="col-xs-6 col-xs-offset-3">
         <br />
         <br />
-        <form action="POST" class="form-horizontal" action="<?=$config['global']['basedir']?>">
+        <form method="POST" class="form-horizontal" action="<?=$config['global']['basedir']?>">
             <div class="form-group">
                 <label for="password" class="control-label col-xs-4">Enter Password: </label>
                 <div class="col-xs-8">
@@ -12,7 +12,7 @@
             <div class="form-group">
                 <label class="control-label col-xs-4"></label>
                 <div class="col-xs-8">
-                    <button class="btn btn-success" style="text-transform: uppercase; ">Login</button>
+                    <button class="btn btn-success" style="text-transform: uppercase; ">Log in</button>
                 </div>
             </div>
         </form>
