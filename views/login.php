dfcb7319565dea348453a9cfcfe38cfb65e3b3b9
Johnathan Tiong <johnathan.tiong@gmail.com>
2015-09-16 17:36:16 +1000

    updated login view

--- views/login.php
+++ views/login.php
@@ -1,5 +1,14 @@
 <div class="container">
     <div class="col-xs-6 col-xs-offset-3">
-        Login Form
+        <br />
+        <br />
+        <form action="POST" class="form-horizontal" action="<?=$config['global']['basedir']?>">
+            <div class="form-group">
+                <label for="password" class="control-label col-xs-3">Enter Password: </label>
+                <div class="col-xs-9">
+                    <input type="password" name="password" placeholder="Please enter your password to continue" />
+                </div>
+            </div>
+        </form>
     </div>
 </div>
